1b50cf108ab684d51f67aaf846b646e107f39e53
JellyChen <451453325@qq.com>
2017-12-25 17:32:34 +0800

    lamp

---
+++ php/lamp/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+service mysqld start
+sleep 1
+# echo "start change privaliege;"
+# spawn mysql -u root -p
+# expect "password:" 
+# send "/r"
+# send "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '' WITH GRANT OPTION;
+# FLUSH PRIVILEGES;\r"
+# send "exit;\r"
+# expect eof
+# echo "end change privaliege;"
+service mysqld status
+supervisord -n
